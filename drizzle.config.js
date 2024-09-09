25d2a802bf778e277414b90f61e08bb1be59e1fa
Dipankar Majumdar <dipmajumdar1991@gmail.com>
2024-09-09 08:18:58 +0530

    updared

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   schema: "./utils/schema.tsx",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://AI-Content-Generator_owner:F6I5aSzxwAYm@ep-super-bread-a110wkok.ap-southeast-1.aws.neon.tech/AI-Content-Generator?sslmode=require",
+    url: NEXT_PUBLIC_DRIZZLE_DB_URL,
   },
 };
