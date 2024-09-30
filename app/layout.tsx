67056600221af7fc8d3b22048623f827f8017d21
Dipankar Majumdar <dipmajumdar1991@gmail.com>
2024-09-30 18:02:42 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const outfit = Outfit({ subsets: ["latin"] });
@@ -20,6 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={outfit.className}>{children}</body>
+        <SpeedInsights />
       </html>
     </ClerkProvider>
   );
